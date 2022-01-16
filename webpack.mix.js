d7b9704714c65684243451bbc6e6386acac301a0
Henrik Kurkela <henrik.kurkela@gmail.com>
2022-01-16 18:28:32 +0200

    Disable BrowserSync notify

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss')
     ])
-    .browserSync('localhost');
-    
+    .browserSync({
+        proxy: 'localhost',
+        notify: false
+    });
