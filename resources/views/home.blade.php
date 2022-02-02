eceaa6062bb737b58829154de0e47c233ae08f61
Henrik Kurkela <henrik.kurkela@gmail.com>
2022-02-02 18:42:55 +0200

    Home style empty tasks list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,9 +22,15 @@
         <div>
             <p class='text-md text-slate-900'>Unfinished tasks</p>
             <div class='flex flex-col gap-4'>
-                @foreach ($tasks as $task)
-                    <x-task :task='$task->task' :id='$task->id' />
-                @endforeach
+                @if (count($tasks) > 0)
+                    @foreach ($tasks as $task)
+                        <x-task :task='$task->task' :id='$task->id' />
+                    @endforeach
+                @else
+                    <p class='w-full bg-slate-300 text-center text-md p-3'>
+                        No tasks unfinished!
+                    </p>
+                @endif
             </div>
         </div>
         <x-task-table :tasks='$tasks' />
